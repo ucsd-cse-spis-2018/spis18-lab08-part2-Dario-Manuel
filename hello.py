b6b175a2acf5243c60e3add4110b43c499579e74
thatbob9001 <urrutiamanuel9@gmail.com>
2018-08-27 13:32:40 -0700

    MU/DA WIP Miles to KM excercise

--- hello.py
+++ hello.py
@@ -6,6 +6,8 @@ def hello():
     return "Whats Up!"
 def ftoc(ftemp):
     return (ftemp-32.0)*(5.0/9.0)
+def mtok(mdist):
+    return (mdist * 1.61)
 
 @app.route('/ftoc/<ftempString>')
 def convertFtoC(ftempString):
@@ -16,6 +18,16 @@ def convertFtoC(ftempString):
         return "In Farenheit: " + ftempString + " In Celsius " + str(ctemp) 
     except ValueError:
         return "Sorry.  Could not convert " + ftempString + " to a number"
+
+@app.route('/mtok/<mdistString>')
+def convertMtoK(mdistString):
+    mdist = 0.0
+    try:
+        mdist = float(mdistString)
+        kdist = mtok(mdist)
+        return "In Miles: " + mdistString + " In Kilometers: " + str(kdist)
+    except ValueError:
+        return "Sorry.  Could not convert " + mdistString + " to a number"
 
 if __name__ == "__main__":
     app.run(port=5000, debug = False)
