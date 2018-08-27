79ca06747d5d6ffde25541983550c9e34f9f433c
thatbob9001 <urrutiamanuel9@gmail.com>
2018-08-27 14:01:11 -0700

    DA/MU WIP Set up for part 2 of webapp.

--- hello.py
+++ hello.py
@@ -1,33 +1,54 @@
-from flask import Flask
+import os
+from flask import Flask, url_for, render_template, request
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
+def render_main():
+    return render_template('home.html')
+
+@app.route('/ctof')
+def render_ctof():
+    return render_template('ctof.html')
+
+@app.route('/ftoc')
+def render_ftoc():
+    return render_template('ftoc.html')
+
+@app.route('/mtokm')
+def render_mtokm():
+    return render_template('mtokm.html')
+    
+@app.route('/ftoc_result')
+def render_ftoc_result():
+    try:
+        ftemp_result = float(request.args['fTemp'])
+        ctemp_result = ftoc(ftemp_result)
+        return render_template('ftoc_result.html', fTemp=ftemp_result, cTemp=ctemp_result)
+    except ValueError:
+        return "Sorry: something went wrong."
+
+@app.route('/ctof_result')
+def render_ctof_result():
+    try:
+        ctemp_result = float(request.args['cTemp'])
+        ftemp_result = ctof(ctemp_result)
+        return render_template('ctof_result.html', cTemp=ctemp_result, fTemp=ftemp_result)
+    except ValueError:
+        return "Sorry: something went wrong."
+
+@app.route('/mtokm_result')
+def render_mtokm_result():
+    try:
+        # You'll need some code here, and maybe some extra parameters in render_template below...
+        return render_template('mtokm.html')
+    except ValueError:
+        return "Sorry: something went wrong."
+
 def hello():
     return "Whats Up!"
 def ftoc(ftemp):
     return (ftemp-32.0)*(5.0/9.0)
 def mtok(mdist):
     return (mdist * 1.61)
-
-@app.route('/ftoc/<ftempString>')
-def convertFtoC(ftempString):
-    ftemp = 0.0
-    try:
-        ftemp = float(ftempString)
-        ctemp = ftoc(ftemp)
-        return "In Farenheit: " + ftempString + " In Celsius " + str(ctemp) 
-    except ValueError:
-        return "Sorry.  Could not convert " + ftempString + " to a number"
-
-@app.route('/mtok/<mdistString>')
-def convertMtoK(mdistString):
-    mdist = 0.0
-    try:
-        mdist = float(mdistString)
-        kdist = mtok(mdist)
-        return "In Miles: " + mdistString + " In Kilometers: " + str(kdist)
-    except ValueError:
-        return "Sorry.  Could not convert " + mdistString + " to a number"
-
 if __name__ == "__main__":
     app.run(port=5000, debug = False)
