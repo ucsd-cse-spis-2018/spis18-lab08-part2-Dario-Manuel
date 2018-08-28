6e3c1d58973239cb7c432198f9bdb6917a102afd
Manuel Urrutia <murrutia@ucsd.edu>
2018-08-28 14:39:52 -0700

    Fixed spacing on program

--- hello.py
+++ hello.py
@@ -1,23 +1,29 @@
 import os
 from flask import Flask, url_for, render_template, request
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def render_main():
     return render_template('home.html')
 
+
 @app.route('/ctof')
 def render_ctof():
     return render_template('ctof.html')
+
 
 @app.route('/ftoc')
 def render_ftoc():
     return render_template('ftoc.html')
 
+
 @app.route('/mtokm')
 def render_mtokm():
     return render_template('mtokm.html')
-    
+
+
 @app.route('/ftoc_result')
 def render_ftoc_result():
     try:
@@ -26,6 +32,7 @@ def render_ftoc_result():
         return render_template('ftoc_result.html', fTemp=ftemp_result, cTemp=ctemp_result)
     except ValueError:
         return "Sorry: something went wrong."
+
 
 @app.route('/ctof_result')
 def render_ctof_result():
@@ -36,6 +43,7 @@ def render_ctof_result():
     except ValueError:
         return "Sorry: something went wrong."
 
+
 @app.route('/mtokm_result')
 def render_mtokm_result():
     try:
@@ -45,13 +53,22 @@ def render_mtokm_result():
     except ValueError:
         return "Sorry: something went wrong."
 
+
 def hello():
     return "What's Up!"
+
+
 def ftoc(ftemp):
-    return (ftemp-32.0)*(5.0/9.0)
+    return (ftemp - 32.0) * (5.0 / 9.0)
+
+
 def ctof(ctemp):
-    return (ctemp*(9/5)) + 32
+    return (ctemp * (9 / 5)) + 32
+
+
 def mtokm(mdist):
     return (mdist * 1.61)
+
+
 if __name__ == "__main__":
-    app.run(port=5000, debug = False)
+    app.run(port=5000, debug=False)
